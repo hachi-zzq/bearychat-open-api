ced0ca5e4a79ff6e5036b9d1e6c6b46a679c3142
朱正钱 <hachi.zzq@gmail.com>
2018-09-28 16:11:15 +0800

    处理请求异常

--- src/Channel/Client.php
+++ src/Channel/Client.php
@@ -2,11 +2,16 @@
 
 namespace Hachi\Bearychat\Channel;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\TransferException;
 use Hachi\Bearychat\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
     const LIST = 'channel.list';
+
+    const INIFO = 'channel.info';
 
     /**
      * 频道列表.
@@ -20,5 +25,21 @@ class Client extends BaseClient
     public function list()
     {
         return $this->httpGet(self::LIST);
+    }
+
+
+    public function info($channelId)
+    {
+        try{
+            $respsone = $this->httpGet(self::INIFO,[
+                'channel_id'=>$channelId
+            ]);
+        }catch (TransferException $exception){
+            $re = getResponseFromGuzzleException($exception);
+            dd($re);
+        }
+
+        dd($repsone);
+
     }
 }
